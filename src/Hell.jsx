18d33d5afede4774100cf7115997fd36e7cf6d70
mai-VAIBHAV <vaibhavtiwari10505@gmail.com>
2025-07-22 10:48:28 +0530

    added the hell.jsx

---
+++ src/Hell.jsx
@@ -0,0 +1,7 @@
+import React from "react"; // ✅ Required in some setups (like older Vite/Babel configs)
+
+function Random() {
+  return <h1>choooote bachi</h1>;
+}
+
+export default Random;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import Random from "./Hell.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <Random />
   </React.StrictMode>
 );
